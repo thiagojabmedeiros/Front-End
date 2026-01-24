47cda7e9ae7c11dd6fd362c1d2a69c2639ea433b
Thiago Medeiros <thiagojabm@gmail.com>
2026-01-24 13:40:44 -0300

    variaveis: const, let e var

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,2 +1,64 @@
-console.log("Hello, world!")
+// comentários se escrevem com "//" no início da linha
+
+/* 
+comentários de multiplas 
+linhas por que assim fica 
+melhor visivelmente
+*/
+
+// ponto e vírgula é obrigatório
+
+console.log("Hello, world!");
 console.log("Olá, mundo!")
+
+// variáveis sem valor
+var user
+console.log(user)
+
+// variáveis com valor 
+var email = "thiago123@gmail.com"
+console.log(email)
+
+email = "rodrigo2123@hotmail.com"
+console.log(email)
+
+// case senstive
+var product = "Teclado gamer"
+var Product = "Mouse gamer"
+
+console.log(product)
+console.log(Product)
+
+/*
+let e var são usadas para criar variáveis
+porém o let é mais restritivo do que o var
+não permitindo que haja repetições de let 
+com o mesmo nome, substituindo-as.
+Já o const é uma variável que permanecerá 
+inalterada.
+*/
+const a = 10
+console.log(a)
+
+/*
+Recomenda-se o uso de let e de const, pois
+o uso de var possui escopo global e 
+pode causar problemas podendo ser 
+acessada da seguinte forma:
+*/
+{
+    // escopo de bloco
+    var x = 10
+}
+//escopo global
+console.log(x)
+
+// não podemos criar variáveis iniciando com número
+// evitar usar acentos e caractéres especiais
+// recomendado criar variáveis com nome inglês
+// utilizar padrão camelCase:
+let productName = "camel"
+console.log(productName)
+//utilizar padrão snake_case:
+let product_name = "snake"
+console.log(product_name)
