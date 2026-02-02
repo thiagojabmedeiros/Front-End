ae001a61f05b51d271aea6113995b6a49169ecb6
Thiago Medeiros <thiagojabm@gmail.com>
2026-02-02 11:23:58 -0300

    creating removed message function

--- lista_de_compras/scripts.js
+++ lista_de_compras/scripts.js
@@ -4,30 +4,33 @@ const buttonAdd = document.querySelector("form button")
 const ul = document.querySelector("ul")
 let index = 0
 
-
-
+// adding elements
 form.addEventListener("submit", (e) => {
     e.preventDefault()
+
     // catching the input name
-    const newItemName = inputAdd.value
+    // trim() function removes all the spaces in the beginning and in the end of the sring
+    const newItemName = inputAdd.value.trim()
     if (newItemName == "") {
+        alert("This is not a valid input")
         return
     }
+    const safeId = newItemName.replaceAll(" ", "") + index
 
     // creating list element
     const li = document.createElement("li")
 
     // creating label
     const label = document.createElement("label")
-    label.htmlFor = newItemName
+    label.htmlFor = safeId
 
     // creating input checkbox
     const inputCheck = document.createElement("input")
-    inputCheck.type = "checkbox"
-    inputCheck.id = newItemName
+    inputCheck.id = safeId
     inputCheck.value = newItemName
     index++
     inputCheck.name = "item" + index
+    inputCheck.type = "checkbox"
 
     // creating paragraph
     const paragraph = document.createElement("p")
@@ -36,6 +39,10 @@ form.addEventListener("submit", (e) => {
     //creating button
     const trashButton = document.createElement("button")
     trashButton.type = "button"
+    trashButton.addEventListener("click", () => {
+        li.remove()
+        index--
+    })
 
     // adding the elments to the label
     label.append(inputCheck, paragraph, trashButton)
@@ -48,5 +55,17 @@ form.addEventListener("submit", (e) => {
 
     // cleaning input value after submit the new item
     inputAdd.value = ""
-
 })
+
+// adding message in the screen
+const removedItemMessage = document.querySelector(".removedItem")
+function showRemovedItemMessage() {
+    removedItemMessage.classList.add("show")
+}
+
+// removing the message from screen
+const removedItemCloseButton = removedItemMessage.querySelector("button")
+removedItemCloseButton.addEventListener("click", () => {
+    removedItemMessage.classList.remove("show")
+})
+
