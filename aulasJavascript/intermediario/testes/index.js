48a45c705d949b5a64e3f577091df2a2d819a034
Thiago Medeiros <thiagojabm@gmail.com>
2026-02-19 18:12:52 -0300

    creating project

--- aulasJavascript/intermediario/testes/index.js
+++ aulasJavascript/intermediario/testes/index.js
@@ -1,7 +1,18 @@
-const max = document.querySelector("#max").value
-const min = document.querySelector("#min").value
+const form = document.querySelector("form")
+const firstNumber = document.querySelector("#firstNumber")
+const secondNumber = document.querySelector("#secondNumber")
+const result = document.querySelector("#result h2")
 
-let randomNumber = Math.floor(Math.random() * (max - min) + min)
 
-const randomNum = document.querySelector("#randomNum")
-randomNum.textContent = randomNumber
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    result.textContent = randomNum()
+})
+
+let randomNum = function() {
+    let start = Number(firstNumber.value)
+    let end = Number(secondNumber.value)
+    let newRandom = Math.floor(Math.random() * (end - start)) + start
+    return newRandom
+}
