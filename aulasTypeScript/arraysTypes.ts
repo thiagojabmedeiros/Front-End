66f9662d0faa895b466f50098b38c416917e8686
Thiago Medeiros <thiagojabm@gmail.com>
2026-02-24 17:18:44 -0300

    arrays type

---
+++ aulasTypeScript/arraysTypes.ts
@@ -0,0 +1,16 @@
+let arrAny: any[]
+let arrStr: string[]
+let arrNum: number[]
+
+// any array type
+arrAny = [123, "thiago", true, 242]
+
+// string array type
+arrStr = ["thiago", "medeiros", "jose"]
+arrStr = [123, 23214, 4353]
+arrStr = ["bruno", 213, false]
+
+// number array type
+arrNum = [1,23,54,65]
+arrNum = ["thig0", "343", "hello"]
+arrNum = ["233", 2132, true]
