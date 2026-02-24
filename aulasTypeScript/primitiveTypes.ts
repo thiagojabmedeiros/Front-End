c8951dd2af6c97ee1067b85d69df9dbbc70c0506
Thiago Medeiros <thiagojabm@gmail.com>
2026-02-24 17:14:25 -0300

    typescript lessons

---
+++ aulasTypeScript/primitiveTypes.ts
@@ -0,0 +1,25 @@
+// types
+let anyt: any
+let strs: string
+let num: number
+let bool: boolean
+
+// any type
+anyt = "thiago"
+anyt = 123
+anyt = false
+
+// string type
+strs = "thiago"
+strs = 123
+strs = true
+
+// number type
+num = 123
+num = "thiago"
+num = false
+
+// bool type
+bool = true
+bool = 123
+bool = "thiago"
