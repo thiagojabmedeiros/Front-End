66a93d378fcd1b5468a6acbaa773f1860510f134
Thiago Medeiros <thiagojabm@gmail.com>
2026-02-24 17:30:32 -0300

    union types

---
+++ aulasTypeScript/unionTypes.ts
@@ -0,0 +1,20 @@
+// creating an union between:
+let unionSN: string | number
+
+unionSN = "thiago"
+unionSN = 1234
+unionSN = true
+
+// creating an union between three types:
+let unionSNB: string | number | boolean
+
+unionSNB = "thiago"
+unionSNB = 1234
+unionSNB = false
+
+
+let unionX: string | null
+unionX = "thiago"
+unionX = true
+unionX = null
+unionX = 2142
