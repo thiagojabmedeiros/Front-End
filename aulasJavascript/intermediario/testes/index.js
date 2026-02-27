666e2597a9608ed492eb3a332a9c584e94c19fa1
Thiago Medeiros <thiagojabm@gmail.com>
2026-02-26 21:50:09 -0300

    tackling functions

--- aulasJavascript/intermediario/testes/index.js
+++ aulasJavascript/intermediario/testes/index.js
@@ -1,18 +1,25 @@
-const form = document.querySelector("form")
-const firstNumber = document.querySelector("#firstNumber")
-const secondNumber = document.querySelector("#secondNumber")
-const result = document.querySelector("#result h2")
+// const form = document.querySelector("form")
+// const firstNumber = document.querySelector("#firstNumber")
+// const secondNumber = document.querySelector("#secondNumber")
+// const result = document.querySelector("#result h2")
 
 
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    result.textContent = randomNum()
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     result.textContent = randomNum()
+// })
+
+// let randomNum = function() {
+//     let start = Number(firstNumber.value)
+//     let end = Number(secondNumber.value)
+//     let newRandom = Math.floor(Math.random() * (end - start)) + start
+//     return newRandom
+// }
+const start = document.querySelector("#start")
+console.log(start)
+
+start.addEventListener("input", () => {
+    let _value = start.value.replace(/\D+/g, "")
+    start.value = _value
 })
-
-let randomNum = function() {
-    let start = Number(firstNumber.value)
-    let end = Number(secondNumber.value)
-    let newRandom = Math.floor(Math.random() * (end - start)) + start
-    return newRandom
-}
--- sorteador_de_numeros/index.js
+++ sorteador_de_numeros/index.js
@@ -3,22 +3,48 @@ const quantity = document.querySelector("#numbers")
 const start = document.getElementById("start")
 const end = document.getElementById("end")
 const button = document.querySelector("#appButton button")
-let randomNumber1
-let randomNumber2
-let randomNumber3
 
 quantity.value = 1
 start.value = 1
 end.value = 100
 
+quantity.addEventListener("input", () => {
+    let _value = quantity.value.replace(/\D+/g, "")
+    quantity.value = _value
+    const valid = [1, 2, 3]
+    if (!valid.includes(Number(quantity.value))) {
+        quantity.value = ""
+    }
+})
+start.addEventListener("input", () => {
+    let _value = start.value.replace(/\D+/g, "")
+    start.value = _value
 
+    if (Number(start.value) > 99) {
+        start.value = 99
+    } else if (Number(start.value) < 0) {
+        start.value = ""
+    } 
+
+    return start.value
+})
+end.addEventListener("input", () => {
+    let _value = end.value.replace(/\D+/g, "")
+    end.value = _value
+    if (Number(end.value) <= Number(start.value)) {
+        end.value = Number(start.value) + 2
+    } else if (Number(end.value) > 100) {
+        end.value = 100
+    }
+})
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     try {
-        randomNumber1 = Math.floor(Math.random() * Number(end.value - start.value)) + Number(start.value)
-        randomNumber2 = Math.floor(Math.random() * Number(end.value - start.value)) + Number(start.value)
-        randomNumber3 = Math.floor(Math.random() * Number(end.value - start.value)) + Number(start.value)
+        removeApp()
+        let randomNumber1 = Math.floor(Math.random() * Number(end.value - start.value)) + Number(start.value)
+        let randomNumber2 = Math.floor(Math.random() * Number(end.value - start.value)) + Number(start.value)
+        let randomNumber3 = Math.floor(Math.random() * Number(end.value - start.value)) + Number(start.value)
 
         if (Number(quantity.value) === 1) {
             console.log(randomNumber1)
@@ -39,3 +65,7 @@ form.addEventListener("submit", (e) => {
         console.log(e)
     }
 })
+
+function removeApp() {
+
+}
