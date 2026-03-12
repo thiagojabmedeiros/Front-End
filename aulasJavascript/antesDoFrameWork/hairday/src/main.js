3fbcfae5e6141a65959e81e50610fe385eb76d7c
Thiago Medeiros <thiagojabm@gmail.com>
2026-03-12 11:45:59 -0300

    packages

---
+++ aulasJavascript/antesDoFrameWork/hairday/src/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// dayjs 
+import "./libs/dayjs.js"
+
+// css
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
+
+import dayjs from "dayjs"
+console.log(dayjs().format("HH:mm"))
