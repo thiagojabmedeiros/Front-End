ae4ff791be13f18e66b478f3631687b458726d95
Thiago Medeiros <thiagojabm@gmail.com>
2026-07-09 20:08:13 -0300

    studying react

--- aulasReact/classroom/src/App.tsx
+++ aulasReact/classroom/src/App.tsx
@@ -1,5 +1,30 @@
+import "./global.css"
+import { Button } from "./components/button"
+import { useState } from "react"
+
+import styles from "./app.module.css"
+
+// import { useMessage } from "./hooks/useMessage"
+
 function App() {
-    return <h1>Hello, React!</h1>
+    // const {show} = useMessage({name: "thiago", age: 22})
+    const [count, setCount] = useState(0)
+
+    function addN(){
+        setCount(count + 1)
+    }
+    function subN() {
+        if (count > 0) {
+        setCount(count - 1)
+        }
+    }
+    return ( 
+    <div className={styles.container}>
+        <Button name="adicionar"  onClick={addN}/>
+        <span>{count}</span>
+        <Button name="remover" onClick={subN}/>
+    </div>
+    )
 }
 
 export { App }
---
+++ aulasReact/classroom/src/hooks/useMessage.ts
@@ -0,0 +1,13 @@
+type Props = {
+    name: string,
+    age: number
+}
+
+function useMessage({ name, age }: Props) {
+    function show(message: string) {
+        console.log(name, age, message)
+    }
+    return { show }
+}
+
+export { useMessage }
