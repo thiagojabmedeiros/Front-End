5943f473285d1269af5490dc2bac493e57878646
Thiago Medeiros <thiagojabm@gmail.com>
2026-07-26 20:12:49 -0300

    responsivity using tailwind

--- aulasReact/tailwindcss/src/sections/End.tsx
+++ aulasReact/tailwindcss/src/sections/End.tsx
@@ -1,7 +1,7 @@
 function End() {
     return (
         <div className="w-15 h-12 bg-red-400 hover:bg-purple-500 dark:bg-green-300 dark:hover:bg-indigo-500">
-            <h1 className="text-left text-2xl text-purple-500 hover:text-red-400">end</h1>
+            <h1 className="text-left text-sm sm:text-base md:text-lg lg:text-xl text-purple-500 hover:text-red-400">end</h1>
         </div>
     )
 }
