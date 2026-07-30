2ffd3943379f3718c206a775642ac015583661fd
Thiago Medeiros <thiagojabm@gmail.com>
2026-07-29 23:18:56 -0300

    creating new 3d component

--- aulasReact/threejs/src/components/Computer.tsx
+++ aulasReact/threejs/src/components/Computer.tsx
@@ -1,22 +1,30 @@
 import styles from "./computer.module.css"
 
 import { Canvas, useLoader } from "@react-three/fiber"
-import { Environment, Html, OrbitControls, PresentationControls } from "@react-three/drei"
+import { Environment, PresentationControls } from "@react-three/drei"
 import { GLTFLoader } from "three/examples/jsm/Addons.js"
 
-function Object() {
-    const object = useLoader(GLTFLoader, "/inclinedComputer.glb")
+
+function ComputerModel() {
+    const object = useLoader(GLTFLoader, "/inclinedcomputerx.glb")
 
     return (
-        <PresentationControls polar={[-0.3,0.4]} azimuth={[-0.6,0.6]}>
-            <primitive object={object.scene}>
-                <Html className={styles.screen} occlude position={[0.023,0.62,-0.23]} transform distanceFactor={1.5} rotation={[0.07,0,0]}>
-                    <div>
-                        <h1>Hello, Three!</h1>
-                    </div>
-                </Html>
+        <PresentationControls polar={[-0.6, 0.4]} azimuth={[-0.6, 0.6]}>
+            <primitive scale={1.25} object={object.scene}>
+                <group 
+                position={[0,0.8,-0.5]} 
+                rotation={[0.14,0,0]}>
+                    <Terminal />
+                </group>
             </primitive>
-        // </PresentationControls>
+        </PresentationControls>
+    )
+}
+function Terminal() {
+    const object = useLoader(GLTFLoader, "/terminal.glb")
+
+    return (
+        <primitive object={object.scene} />
     )
 }
 
@@ -25,13 +33,8 @@ function Computer() {
         <div className={styles.container}>
             <Canvas>
                 <Environment preset="dawn"/>
-                <directionalLight />
-                    {/* <mesh position={[1,2,2]}>
-                        <sphereGeometry args={[1,6,10]}/>
-                        <meshPhongMaterial color="red" />
-                    </mesh> */}
-                <Object />
-                {/* <OrbitControls/> */}
+
+                <ComputerModel />
             </Canvas>
         </div>
     )
